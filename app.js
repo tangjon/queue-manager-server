f054b8d1c827ca1369c6f456cc80068144336a5b
Jonathan Tang <tangjon@hotmail.com>
2018-07-09 21:26:11 -0700

    finish add, update, create for user

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const app = express();
 
 
 // ROUTES
-const home = require('./routes/home.js')
-const incident = require('./routes/incident.js')
-const user = require('./routes/user.js')
+const home = require('./routes/home.js');
+const incident = require('./routes/incident.js');
+const user = require('./routes/user.js');
 
 // MIDDLEWARE
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
 
-app.use('/api/',home)
-app.use('/api/incident/', incident)
-app.use('/api/users/', user)
+app.use('/api/',home);
+app.use('/api/incident/', incident);
+app.use('/api/users/', user);
 
 
 var server = app.listen(8081, function () {
 
-   var host = server.address().address
-   var port = server.address().port
+   var host = server.address().address;
+   var port = server.address().port;
 
    console.log("Example app listening at http://localhost:%s", port)
-})
+});
--- routes/user.js
+++ routes/user.js
@@ -4,22 +4,20 @@ const router = express.Router();
 
 const connection = require('../sqlconfig');
 
+const USER_PARAMS = [
+    "i_number",
+    "first_name",
+    "last_name",
+    "is_available",
+    "usage_percent",
+    "current_q_days",
+    "incident_threshold",
+];
+
 // GET ALL USERS
 router.get('/', function (req, res) {
     let query = 'SELECT * FROM user';
-    connection.query(query, function (error, results, fields) {
-        if (error) throw error;
-        if (results.length == 0) {
-            res.send("No Data")
-        } else {
-            res.send(JSON.stringify(results))
-        }
-    });
-});
-// TODO ADD USER
-router.post('/', function (req, res) {
-    let query = 'SELECT * FROM user';
-    connection.query(query, function (error, results, fields) {
+    connection.query(query, function (error, results) {
         if (error) throw error;
         if (results.length === 0) {
             res.send("No Data")
@@ -27,17 +25,50 @@ router.post('/', function (req, res) {
             res.send(JSON.stringify(results))
         }
     });
+});
+router.post('/', function (req, res) {
+    let body = req.body;
+    USER_PARAMS.forEach(element => {
+        if (body[element] == null) {
+            const error_msg = `Missing '${element}' parameter`;
+            res.status(400).send({
+                'error': error_msg
+            });
+            throw Error(error_msg)
+        }
+    });
+    let query = `INSERT INTO qmtooldb.user (i_number, first_name, last_name, is_available,usage_percent,current_q_days,incident_threshold) 
+    VALUES (${connection.escape(body['i_number'])}, 
+        ${connection.escape(body['first_name'])},  
+        ${connection.escape(body['last_name'])}, 
+        ${connection.escape(body['is_available'])},
+        ${connection.escape(body['usage_percent'])},
+        ${connection.escape(body['current_q_days'])},
+        ${connection.escape(body['incident_threshold'])})`;
+
+    connection.query(query, function (error) {
+            if (error) {
+                switch (error.code) {
+                    case "ER_DUP_ENTRY":
+                        res.sendStatus(409);
+                        break
+                }
+            } else {
+                res.status(201).location(req.baseUrl + '/' + connection.escape(body['i_number'])).send();
+            }
+        }
+    )
 });
 
 // GET SPECIFIC USER
 router.get('/:id/', function (req, res) {
     let query = 'SELECT * FROM `qmtooldb`.`user` where i_number =' + connection.escape(req.params['id']);
-    connection.query(query, function (error, results, fields) {
+    connection.query(query, function (error, results) {
         if (error) throw error;
         if (results.length === 0) {
-            res.status(404).send("No Data")
+            res.sendStatus(404)
         } else {
-            res.status(404).send(JSON.stringify(results))
+            res.status(200).json(results[0])
         }
     });
 });
@@ -45,23 +76,13 @@ router.get('/:id/', function (req, res) {
 // UPDATE USER
 router.put('/:id/', function (req, res) {
     let body = req.body;
-    let params = [
-        "i_number",
-        "first_name",
-        "last_name",
-        "is_available",
-        "usage_percent",
-        "current_q_days",
-        "incident_threshold",
-    ];
-
-    params.forEach(element => {
+    USER_PARAMS.forEach(element => {
         if (body[element] == null) {
             const error_msg = `Missing '${element}' parameter`;
             res.status(400).send({
                 'error': error_msg
             });
-            throw Error(error_msg)
+            throw error(error_msg)
         }
     });
     let query = `UPDATE qmtooldb.user SET 
@@ -74,12 +95,11 @@ router.put('/:id/', function (req, res) {
         incident_threshold = ${connection.escape(body['incident_threshold'])} 
         WHERE i_number = ${connection.escape(req.params.id)}
     `;
-    connection.query(query, function (error, results, fields) {
+    connection.query(query, function (error, results) {
         if (error) {
             res.status(400).json({
                 'error': error.message
             });
-            throw error
         }
         if (results.affectedRows === 0) {
             res.status(404).json({
