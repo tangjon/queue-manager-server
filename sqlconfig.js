44417bdb5edd6d541f1e42fa3cccb76674bf746c
Jonathan <jonathan.tang01@sap.com>
2018-07-20 11:29:04 -0700

    globally allow multipleStatements

--- sqlconfig.js
+++ sqlconfig.js
@@ -1,4 +1,6 @@
 const config = require('./SECRET.js');
+// allow multiple statments in config
+config['multipleStatements'] = true;
 const connection = require('mysql').createConnection(config);
 connection.connect();
 
