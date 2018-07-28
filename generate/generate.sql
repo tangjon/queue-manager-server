64785a5ed9dea1863b4ea3e406950374ffe7a309
Jonathan Tang <tangjon@hotmail.com>
2018-07-28 16:35:18 -0700

    update init and generate sql scripts

--- generate/generate.sql
+++ generate/generate.sql
@@ -1,110 +1,110 @@
-USE qmtooldb;
+USE qmtooldb_dev;
 -- ======================
 -- GENERATE PRODUCTS
 -- ======================
 SET @product = "NW";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`product_id`,`short_name`)
 VALUES
 ('1',@product);
-ALTER TABLE qmtooldb.user_supports_product 
+ALTER TABLE user_supports_product
 ADD NW INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT NW FOREIGN KEY (`NW`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT NW FOREIGN KEY (`NW`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "MS";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`product_id`,`short_name`)
 VALUES
 ('2',@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD MS INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT MS FOREIGN KEY (`MS`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT MS FOREIGN KEY (`MS`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "BW4";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`product_id`,`short_name`)
 VALUES
 ('3',@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD BW4 INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT BW4 FOREIGN KEY (`BW4`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT BW4 FOREIGN KEY (`BW4`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "DSM";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`product_id`,`short_name`)
 VALUES
 ('4',@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD DSM INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT DSM FOREIGN KEY (`DSM`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT DSM FOREIGN KEY (`DSM`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "BFC_EA_IC_FIM";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`short_name`)
 VALUES
 (@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD BFC_EA_IC_FIM INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT BFC_EA_IC_FIM FOREIGN KEY (`BFC_EA_IC_FIM`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT BFC_EA_IC_FIM FOREIGN KEY (`BFC_EA_IC_FIM`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "LOD_ANA_PL";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`short_name`)
 VALUES
 (@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD LOD_ANA_PL INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT LOD_ANA_PL FOREIGN KEY (`LOD_ANA_PL`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT LOD_ANA_PL FOREIGN KEY (`LOD_ANA_PL`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "PCM";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`short_name`)
 VALUES
 (@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD PCM INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT PCM FOREIGN KEY (`PCM`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT PCM FOREIGN KEY (`PCM`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "RTC";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`short_name`)
 VALUES
 (@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD RTC INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT RTC FOREIGN KEY (`RTC`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT RTC FOREIGN KEY (`RTC`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "SA";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`short_name`)
 VALUES
 (@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD SA INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT SA FOREIGN KEY (`SA`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT SA FOREIGN KEY (`SA`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 -- =====================
 -- GENERATE USERS
 -- =====================
-INSERT INTO `qmtooldb`.`user`(`user_id`,
+INSERT INTO `user`(`user_id`,
 `first_name`,
 `last_name`,
 `is_available`,
@@ -122,37 +122,37 @@ INSERT INTO `qmtooldb`.`user`(`user_id`,
 -- SET QM
 -- =====================
 
-INSERT INTO `qmtooldb`.`qmuser` (`user_id`) VALUES ('i100000');
+INSERT INTO `qmuser` (`user_id`) VALUES ('i100000');
 
 -- ====================
 -- GENERATE ACTIONS
 -- ====================
-INSERT INTO `qmtooldb`.`action`(`action_id`,
+INSERT INTO `action`(`action_id`,
 `description`)VALUES(1,
 "Custom Message");
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
 (2,
 "Incident Assigned");
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
 (3,
 "Incident Unassigned");
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
 (4,
 "Availability Changed");
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
@@ -160,7 +160,7 @@ VALUES
 "Queue Days Changed");
 
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
@@ -168,7 +168,7 @@ VALUES
 "Product Support Changed");
 
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
--- generate/generate_test.sql
+++ generate/generate_test.sql
@@ -1,110 +1,110 @@
-USE qmtooldb;
+USE qmtooldb_dev;
 -- ======================
 -- GENERATE PRODUCTS
 -- ======================
 SET @product = "NW";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`product_id`,`short_name`)
 VALUES
 ('1',@product);
-ALTER TABLE qmtooldb.user_supports_product 
+ALTER TABLE user_supports_product
 ADD NW INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT NW FOREIGN KEY (`NW`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT NW FOREIGN KEY (`NW`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "MS";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`product_id`,`short_name`)
 VALUES
 ('2',@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD MS INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT MS FOREIGN KEY (`MS`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT MS FOREIGN KEY (`MS`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "BW4";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`product_id`,`short_name`)
 VALUES
 ('3',@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD BW4 INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT BW4 FOREIGN KEY (`BW4`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT BW4 FOREIGN KEY (`BW4`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "DSM";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`product_id`,`short_name`)
 VALUES
 ('4',@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD DSM INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT DSM FOREIGN KEY (`DSM`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT DSM FOREIGN KEY (`DSM`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "BFC_EA_IC_FIM";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`short_name`)
 VALUES
 (@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD BFC_EA_IC_FIM INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT BFC_EA_IC_FIM FOREIGN KEY (`BFC_EA_IC_FIM`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT BFC_EA_IC_FIM FOREIGN KEY (`BFC_EA_IC_FIM`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "LOD_ANA_PL";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`short_name`)
 VALUES
 (@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD LOD_ANA_PL INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT LOD_ANA_PL FOREIGN KEY (`LOD_ANA_PL`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT LOD_ANA_PL FOREIGN KEY (`LOD_ANA_PL`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "PCM";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`short_name`)
 VALUES
 (@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD PCM INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT PCM FOREIGN KEY (`PCM`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT PCM FOREIGN KEY (`PCM`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "RTC";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`short_name`)
 VALUES
 (@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD RTC INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT RTC FOREIGN KEY (`RTC`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT RTC FOREIGN KEY (`RTC`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 SET @product = "SA";
-INSERT INTO `qmtooldb`.`product`
+INSERT INTO `product`
 (`short_name`)
 VALUES
 (@product);
-ALTER TABLE qmtooldb.user_supports_product
+ALTER TABLE user_supports_product
 ADD SA INT(11) NULL DEFAULT NULL,
-ADD CONSTRAINT SA FOREIGN KEY (`SA`) REFERENCES `qmtooldb`.`product` (`product_id`)
+ADD CONSTRAINT SA FOREIGN KEY (`SA`) REFERENCES `product` (`product_id`)
 ON DELETE SET NULL
 ON UPDATE CASCADE;
 
 -- =====================
 -- GENERATE USERS
 -- =====================
-INSERT INTO `qmtooldb`.`user`(`user_id`,
+INSERT INTO `user`(`user_id`,
 `first_name`,
 `last_name`,
 `is_available`,
@@ -118,7 +118,7 @@ INSERT INTO `qmtooldb`.`user`(`user_id`,
 "10",
 3);
 
-INSERT INTO `qmtooldb`.`user`(`user_id`,
+INSERT INTO `user`(`user_id`,
 `first_name`,
 `last_name`,
 `is_available`,
@@ -132,7 +132,7 @@ INSERT INTO `qmtooldb`.`user`(`user_id`,
 "10",
 3);
 
-INSERT INTO `qmtooldb`.`user`(`user_id`,
+INSERT INTO `user`(`user_id`,
 `first_name`,
 `last_name`,
 `is_available`,
@@ -147,7 +147,7 @@ INSERT INTO `qmtooldb`.`user`(`user_id`,
 3);
 
 
-INSERT INTO `qmtooldb`.`user`(`user_id`,
+INSERT INTO `user`(`user_id`,
 `first_name`,
 `last_name`,
 `is_available`,
@@ -167,37 +167,37 @@ INSERT INTO `qmtooldb`.`user`(`user_id`,
 -- SET QM
 -- =====================
 
-INSERT INTO `qmtooldb`.`qmuser` (`user_id`) VALUES ('i100000');
+INSERT INTO `qmuser` (`user_id`) VALUES ('i100000');
 
 -- ====================
 -- GENERATE ACTIONS
 -- ====================
-INSERT INTO `qmtooldb`.`action`(`action_id`,
+INSERT INTO `action`(`action_id`,
 `description`)VALUES(1,
 "Custom Message");
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
 (2,
 "Incident Assigned");
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
 (3,
 "Incident Unassigned");
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
 (4,
 "Availability Changed");
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
@@ -205,7 +205,7 @@ VALUES
 "Queue Days Changed");
 
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
@@ -213,7 +213,7 @@ VALUES
 "Product Support Changed");
 
 
-INSERT INTO `qmtooldb`.`action`
+INSERT INTO `action`
 (`action_id`,
 `description`)
 VALUES
@@ -243,67 +243,67 @@ SET @MIN_PRODUCT_ID = 1;
 SET @MAX_PRODUCT_ID = 3;
 SET @logger = 'i100000';
 SET @dummy_user = 'i100000';
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
 
 SET @dummy_user = 'i100001';
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
 
 
 
 SET @dummy_user = 'i100002';
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
 
 
 SET @dummy_user = 'i100003';
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
-INSERT INTO qmtooldb.incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
-INSERT INTO `qmtooldb`.`actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
+INSERT INTO incident (user_id, product_id) SELECT @dummy_user, FLOOR(RAND()*(@MAX_PRODUCT_ID-@MIN_PRODUCT_ID+1)+@MIN_PRODUCT_ID);
+INSERT INTO `actionentrylog` (`logger_id`,`action_id`,`affected_user_id`,`detail`) SELECT @logger,2,@dummy_user, "TEST";
 
 
 -- =====================
 -- TOGGLE USERS
 -- =====================
-UPDATE `qmtooldb`.`user` SET `is_available` = "1" WHERE `user_id` = 'i100000';
+UPDATE `user` SET `is_available` = "1" WHERE `user_id` = 'i100000';
 
 -- =====================
 -- COUNT THE NUMBER OF INCIDENTS
