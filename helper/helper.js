74370b19ebd9e39286deb3a54027159dcdee75c0
Jonathan <jonathan.tang01@sap.com>
2018-07-30 14:48:44 -0700

    logs are output when there is an error

--- helper/helper.js
+++ helper/helper.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 module.exports.GET = function (response, data) {
     return response.status(200).json({
         "code": 200,
@@ -41,8 +43,23 @@ module.exports.DELETE = function (response, results) {
 };
 
 module.exports.ERROR = function (response, error) {
-    console.error(error);
-    console.log(response);
+    // log-file_YYYY-MM-DD_HH:MM:SS
+    const date = new Date();
+    let fileName = `log-file_${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}_${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}.txt`
+    // fs.writeFile("logs/" + fileName, error, function (err) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+
+    //     console.log("log file generated: " + fileName);
+    // });
+
+    var stream = fs.createWriteStream("logs/" + fileName);
+    stream.once('open', function (fd) {
+        console.log("log file generated: " + fileName);
+        stream.write(JSON.stringify(error));
+        stream.write("\n");
+    });
     response.status(404).json({
         "message": error.message
     });
