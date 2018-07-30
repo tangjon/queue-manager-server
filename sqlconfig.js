26f559e0b3f758c31c687a71931bbc5922c15e73
Jonathan <jonathan.tang01@sap.com>
2018-07-30 11:08:42 -0700

    have sql config point to different databases

--- sqlconfig.js
+++ sqlconfig.js
@@ -1,4 +1,18 @@
-const config = require('./SECRET.js');
+const config_import = require('./SECRET.js');
+let config = config_import.dev;
+switch (process.argv[2]) {
+    case "dev":
+        config = config_import.dev
+        break;
+    case "prod":
+        config = config_import.prod
+        break;
+    case "sandbox":
+         config = config_import.sandbox
+    default:
+        config = config_import.dev
+        break;
+}
 // allow multiple statments in config
 config['multipleStatements'] = true;
 const connection = require('mysql').createConnection(config);
